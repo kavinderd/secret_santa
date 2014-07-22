065e345e395de6eb113ddfc5776bdbe87f2043b7
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-07-22 16:56:41 -0700

    setup folder structure

--- test/test.rb
+++ test/test.rb
@@ -4,3 +4,14 @@ $:.unshift('..')
 $:.unshift('../lib')
 
 require 'secret_santa'
+require 'minitest/autorun'
+
+class SecretSantaTest < Minitest::Test
+
+  def test_accept_name_parameters
+  	ss =SecretSanta.new
+  	ss.input_record('Test', 'Santa', 'santa@northpole.com')
+  	assert_equal ss.records, [['Test', 'Santa', 'santa@northpole.com']]
+  end
+
+end
