64f0e6bc285e8a9a5b600f812a885f3ac1f466d8
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-07-24 12:10:18 -0700

    assign santas

--- lib/secret_santa.rb
+++ lib/secret_santa.rb
@@ -1,12 +1,39 @@
+require 'pry'
+require 'pry-debugger'
 class SecretSanta
-  attr_reader :records
+  attr_reader :records, :assignments
 
   def initialize
     @records = []
   end
 
+
   def input_record(first_name:, last_name: , email:)
+  	raise DuplicateEmailException if @records.flatten.include?(email)
   	@records << [first_name, last_name, email]
   end
 
+  def assign_santas
+  	@assignments = []
+  	while @records.count > 0
+  	  r = @records.shift
+  	  match =nil
+  	  loop do
+	    match = @records.sample
+  	  	match
+  	  	break if r[1] != match[1]
+  	  end
+  	  index = @records.index(match)
+  	  @records.delete_at(index)
+  	  @assignments << [r, match]
+    end
+  end
+
+  private
+
+  def get
+  end
+
 end
+
+class DuplicateEmailException < Exception; end;
--- test/test.rb
+++ test/test.rb
@@ -13,6 +13,32 @@ class SecretSantaTest < Minitest::Test
   	ss.input_record(first_name:'Test', last_name:'Santa', email:'santa@northpole.com')
   	assert_equal ss.records, [['Test', 'Santa', 'santa@northpole.com']]
   end
-  
+
+  def test_accept_multiple_name_parameters
+  	ss =SecretSanta.new
+  	ss.input_record(first_name:'Test', last_name:'Santa', email:'santa@northpole.com')
+  	ss.input_record(first_name: 'Santa', last_name: 'Clause', email: 'clause@northpole.com')
+  	assert_equal ss.records.last, ['Santa', 'Clause', 'clause@northpole.com']
+  end
+
+  def test_dissallow_identical_emails
+  	ss = SecretSanta.new
+  	ss.input_record(first_name:'Test', last_name:'Santa', email:'santa@northpole.com')
+  	assert_raises(DuplicateEmailException) { ss.input_record(first_name:'Clause', last_name:'Santa', email:'santa@northpole.com')}
+  end
+
+  def test_assign_santas
+  	ss = SecretSanta.new
+  	ss.input_record(first_name: 'Mother', last_name: 'Clause', email: 'motherclause@northpole.com')
+  	ss.input_record(first_name: 'Santa', last_name: 'Clause', email: 'santaclause@northpole.com')
+  	ss.input_record(first_name: 'Fred', last_name: 'Clause', email: 'fredclause@northpole.com')
+  	ss.input_record(first_name: 'Mary', last_name: 'Clause', email: 'maryclause@northpole.com')
+  	ss.input_record(first_name: 'Rudolph', last_name: 'Reindeer', email: 'rudolph@northpole.com')
+  	ss.input_record(first_name: 'Short', last_name: 'Elf', email: 'short@northpole.com')
+  	ss.input_record(first_name: 'Tall', last_name: 'Elf', email: 'tall@northpole.com')
+  	ss.input_record(first_name: 'Adam', last_name: 'Kid', email: 'kid@earth.com')
+  	ss.assign_santas
+  	assert_equal 4, ss.assignments.count
+  end
 
 end
